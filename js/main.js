10128b4f03ff16b6dbeb79af1b9af506ce7391f2
brunoeiji151 <brunoeiji151@gmail.com>
2023-05-24 10:08:22 -0300

    Adicionando Função Scroll

--- js/main.js
+++ js/main.js
@@ -30,35 +30,53 @@ function linkAction() {
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*============================ PROGRESS-BAR ==================================*/
-// Espera até que a janela seja totalmente carregada
-window.onload = function () {
-    // Seleciona todos os elementos com a classe "bar" e os armazena em uma variável chamada "bar"
-    let bar = document.querySelectorAll('.bar');
+function progressAnimation() {
+    // Adiciona um event listener para o evento de scroll
+    window.addEventListener('scroll', function () {
+        // Obtém a posição vertical atual da janela
+        var scrollPosition = window.scrollY;
 
-    // Itera sobre cada elemento na variável "bar"
-    bar.forEach((progress) => {
-        // Obtém o valor da porcentagem do atributo "data-value" do elemento atual
-        let value = progress.getAttribute('data-value');
+        // Obtém a posição vertical da seção desejada
+        var section = document.getElementById('scrollProgressBar');
+        var sectionPosition = section.getBoundingClientRect().top;
 
-        // Define a largura da barra de progresso com base no valor da porcentagem
-        progress.style.width = `${value}%`;
+        // Obtém a altura da janela
+        var windowHeight = window.innerHeight;
 
-        // Inicializa uma variável de contagem com o valor zero
-        let count = 0;
+        // Verifica se a posição vertical da seção está visível na janela
+        if (sectionPosition <= windowHeight) {
+            // Seleciona todos os elementos com a classe "bar" e os armazena em uma variável chamada "bar"
+            let bar = document.querySelectorAll('.bar');
 
-        // Configura uma animação que incrementa a contagem e atualiza o texto da porcentagem a cada 15 milissegundos
-        let progressAnimation = setInterval(() => {
-            // Incrementa a contagem em 1
-            count++;
+            // Itera sobre cada elemento na variável "bar"
+            bar.forEach((progress) => {
+                // Obtém o valor da porcentagem do atributo "data-value" do elemento atual
+                let value = progress.getAttribute('data-value');
 
-            // Define o valor do atributo "data-text" com a contagem atual seguida do símbolo de porcentagem
-            progress.setAttribute('data-text', `${count}%`);
+                // Define a largura da barra de progresso com base no valor da porcentagem
+                progress.style.width = `${value}%`;
 
-            // Verifica se a contagem atual é maior ou igual à porcentagem final
-            if (count >= value) {
-                // Se sim, interrompe a animação
-                clearInterval(progressAnimation);
-            }
-        }, 15);
+                // Inicializa uma variável de contagem com o valor zero
+                let count = 0;
+
+                // Configura uma animação que incrementa a contagem e atualiza o texto da porcentagem a cada 15 milissegundos
+                let progressAnimation = setInterval(() => {
+                    // Incrementa a contagem em 1
+                    count++;
+
+                    // Define o valor do atributo "data-text" com a contagem atual seguida do símbolo de porcentagem
+                    progress.setAttribute('data-text', `${count}%`);
+
+                    // Verifica se a contagem atual é maior ou igual à porcentagem final
+                    if (count >= value) {
+                        // Se sim, interrompe a animação
+                        clearInterval(progressAnimation);
+                    }
+                }, 15);
+            });
+        }
     });
-};
+}
+
+// Chame a função progressAnimation quando desejar executar o código
+progressAnimation();
